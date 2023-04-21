67a2ed6a46f0c2144207fed5d0260b122d8075d4
刘芳 <2531994628@qq.com>
2023-04-21 17:50:55 +0800

    第三次提交

--- data/database.py
+++ data/database.py
@@ -3,24 +3,14 @@ import pymongo
 
 class database(object):
 
-    # def __init__(self):
-    #     self.username = "admin"
-    #     self.password = "123456"
-    #     self.host = "127.0.0.1"
-    #     self.port = "27017"
-    #     self.databaseName = "admin"
-    #     self.companyId = None
-    #     self.func_dict = {"-u": self.set_username, "-p": self.set_password, "--host": self.set_host,
-    #                       "--port": self.set_port, "--databaseName": self.set_database}
-
-
-    # 初始化数据库，如果数据库设置了账号密码请使用上面这段代码
     def __init__(self):
-        self.host = "124.221.189.58"
-        self.port = "27017"
-        self.databaseName = "admin"
+        self.username = None
+        self.password = None
+        self.host = None
+        self.port = None
+        self.databaseName = None
         self.companyId = None
-        self.func_dict = {"--host": self.set_host,
+        self.func_dict = {"-u": self.set_username, "-p": self.set_password, "--host": self.set_host,
                           "--port": self.set_port, "--databaseName": self.set_database}
 
     def set_username(self, value):
@@ -51,27 +41,14 @@ class database(object):
             value = value.strip()
         self.func_dict[key](value)
 
-    # def valid(self):
-    #     return self.username is not None and \
-    #         self.password is not None and \
-    #         self.host is not None and \
-    #         self.port is not None and \
-    #         self.databaseName is not None
-
     def valid(self):
-        return self.host is not None and \
-            self.port is not None and \
-            self.databaseName is not None
+        return self.username is not None and \
+               self.password is not None and \
+               self.host is not None and \
+               self.port is not None and \
+               self.databaseName is not None
 
-    # def get_database(self):
-    #     conn = pymongo.MongoClient(
-    #         f'mongodb://{self.username}:{self.password}@{self.host}:{self.port}/{self.databaseName}?authSource=admin')
-    #     print(conn[self.databaseName])
-    #     return conn[self.databaseName]
-
-    # 建立数据库连接
     def get_database(self):
         conn = pymongo.MongoClient(
-                f'mongodb://{self.host}:{self.port}/{self.databaseName}')
-        print(conn[self.databaseName])
+            f'mongodb://{self.username}:{self.password}@{self.host}:{self.port}/{self.databaseName}?authSource=admin')
         return conn[self.databaseName]
--- main.py
+++ main.py
@@ -1,120 +1,22 @@
-# -*- coding: utf-8 -*-
 import getopt
 import logging
-import re
+import os
 import sys
 import time
 import uuid
-import warnings
 
-import numpy as np
 import openai
-import pandas as pd
-import tiktoken as tiktoken
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 from dotenv import load_dotenv
-from flask import Flask, request, Response, json
-from openai.embeddings_utils import get_embedding, cosine_similarity
-from typing import Dict, List, Tuple
+from flask import Flask, request, Response
 
 from data import conversation_service as conv_service
 from data import set_database
 from util.Result import err, log, ok
 
-
 app = Flask(__name__)
 
 load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-# openai.api_key = "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"
-COMPLETIONS_MODEL = ""
-TEXT = ""
-openai.api_key = ""
-openai.api_base = ""
-openai.api_version = ""
-
-
-# 获取指定csv列数据
-df = pd.read_csv("")
-
-df_bills = df[['Product', 'KeyMessage', 'TextCategory', 'Doc_Name', 'Doc_Link']]
-
-
-# 删除多余的空格并清理标点符号来执行一些轻微的数据清理
-def normalize_text(s, sep_token=" \n "):
-    s = re.sub(r'\s+', ' ', s).strip()
-    s = re.sub(r". ,", "", s)
-    s = s.replace("..", ".")
-    s = s.replace(". .", ".")
-    s = s.replace("\n", "")
-    s = s.strip()
-
-    return s
-
-
-# 忽略警告，在下面三行代码中会出现pandas警告，因为我们在对一个数据帧（df_bills）的切片进行更改，可能会影响原始数据帧，使用.loc或.copy都不能消除这种警告。
-warnings.filterwarnings("ignore")
-df_bills.loc[:, "KeyMessage"] = df_bills["KeyMessage"].apply(normalize_text).copy()
-# 删除任何对于令牌限制（~2000 个令牌）来说太长的账单
-tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-df_bills.loc[:, 'n_tokens'] = df_bills['KeyMessage'].apply(lambda x: len(tokenizer.encode(x))).values
-df_bills = df_bills[df_bills.n_tokens < 2000]
-
-
-# 使用文档模型嵌入每个块，搜索列中都有其相应的嵌入向量（curie_search、KeyMessage）
-df_bills['curie_search'] = df_bills["KeyMessage"].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
-
-
-# 通过函数传递将查询嵌入到相应的查询模型，并从上一步中先前嵌入的文档中找到最接近它的嵌入
-def search_docs(df, user_query, to_print=True):
-    embedding = get_embedding(
-        user_query,
-        engine="text-embedding-ada-002"
-    )
-
-    df["similarities"] = df.curie_search.apply(lambda x: cosine_similarity(x, embedding))
-
-    # 按文本分类分组并对每个组应用余弦相似性
-    df_extend = df.groupby("TextCategory").apply(lambda x: x.sort_values("similarities", ascending=False).head(1))
-
-    # 统计分组之后的总条数
-    res = df.sort_values("similarities", ascending=False).head(df_extend.shape[0])
-
-    # if to_print:
-    #     display(res)
-
-    return df_extend
-
-
-# 将输入数据中的每行文本嵌入向量进行预处理，方便进行文本匹配和搜索，主要针对字段KeyMessage，存储为字典
-def compute_doc_embeddings(df: pd.DataFrame) -> Dict[Tuple[str, str], List[float]]:
-    return {
-        idx: get_embedding(r.KeyMessage, COMPLETIONS_MODEL) for idx, r in df.iterrows()
-    }
-
-
-document_embeddings = compute_doc_embeddings(df)
-
-# 查询第一条数据，计算KeyMessage每一个条目的向量值，并输出
-example_entry = list(document_embeddings.items())[0]
-
-
-# 设置文本换行、语言模型
-SEPARATOR = "\n* "
-ENCODING = "gpt2"
-
-encoding = tiktoken.get_encoding(ENCODING)
-separator_len = len(encoding.encode(SEPARATOR))
-
-f"Context separator contains {separator_len} tokens"
-
-
-# 设置温度、最大标记数（回复的最长字符）、模型，针对 CSV 文件查询的限制
-COMPLETIONS_API_PARAMS = {
-    "temperature": 0.0,
-    "max_tokens": 1000,
-    "model": COMPLETIONS_MODEL,
-}
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # 设置内置日志记录器等级
 logger = logging.getLogger('werkzeug')
@@ -123,11 +25,13 @@ logger = logging.getLogger('werkzeug')
 # logger.setLevel(logging.ERROR)
 
 
+# code-davinci-002
+# text-davinci-003
+
 def get_param(jn, key, default_value):
     return jn[key] if key in jn else default_value
 
 
-# 获取uuid
 @app.route("/generate/id", methods=['POST'])
 def generate_id():
     return ok(str(uuid.uuid4()))
@@ -182,7 +86,6 @@ def text_change(cid, idx):
     return ok(None)
 
 
-# 停止响应
 @app.route("/stop/chat/<cid>", methods=['PUT'])
 def stop_chat(cid):
     logger.info(log(f"id:{cid}"))
@@ -192,7 +95,6 @@ def stop_chat(cid):
     return ok({})
 
 
-# 保存uuid
 @app.route("/conv/<cid>", methods=['GET'])
 def conv(cid):
     logger.info(log(f"id:{cid}"))
@@ -200,7 +102,6 @@ def conv(cid):
     return ok(conversation)
 
 
-# 获取提问数据的用户名role和提问数据content
 def get_msgs(conversation):
     msgs = [{"role": "system", "content": ""}]
     for c in conversation["convs"]:
@@ -209,7 +110,6 @@ def get_msgs(conversation):
     return msgs
 
 
-# 设置标题
 @app.route("/chat/title/<cid>", methods=['GET'])
 def chat_title(cid):
     logger.info(log(f"id:{cid}"))
@@ -222,22 +122,11 @@ def chat_title(cid):
         conversation["title"] = ai_text
         conv_service.replace(conversation)
 
-    # 找到第一条用户消息
-    first_user_msg = None
-    for msg in get_msgs(conversation):
-        if msg["role"] == "user":
-            first_user_msg = msg
-            break
-
-    # 将第一条用户消息传递给回调函数作为标题的输入
-    if first_user_msg:
-        user_msg = {"role": first_user_msg["role"], "content": first_user_msg["content"]}
-        return Response(generate_chat(cid, request.args, [user_msg], callback), mimetype='text/event-stream')
-    else:
-        return err("未找到用户消息")
+    msgs = get_msgs(conversation)
+    msgs.append({"role": "user", "content": "为以上对话取一个符合的标题"})
+    return Response(generate_chat(cid, request.args, msgs, callback), mimetype='text/event-stream')
 
 
-# 重新响应
 @app.route("/chat/repeat/<cid>", methods=['GET'])
 def chat_repeat(cid):
     logger.info(log(f"id:{cid}"))
@@ -251,18 +140,10 @@ def chat_repeat(cid):
         conversation["convs"][-1]["suitable"].append(0)
         conv_service.replace(conversation)
 
-    # 获取role为"user"的对话内容
-    user_msgs = []
-    for msg in get_msgs(conversation):
-        if msg["role"] == "user":
-            user_msg = {"role": msg["role"], "content": msg["content"]}
-            user_msgs.append(user_msg)
-
-    # 将对话内容传递给generate_chat函数
-    return Response(generate_chat(cid, request.args, user_msgs, callback), mimetype='text/event-stream')
+    msgs = get_msgs(conversation)
+    return Response(generate_chat(cid, request.args, msgs, callback), mimetype='text/event-stream')
 
 
-# 提问
 @app.route("/chat/<cid>", methods=['GET'])
 def chat(cid):
     if not request.args \
@@ -299,112 +180,36 @@ def chat(cid):
     return Response(generate_chat(cid, request.args, msgs, callback), mimetype='text/event-stream')
 
 
-# 模型输出
 def generate_chat(cid, rjson, msgs, func):
-    last_message_content = ""
-    for msg in tuple(msgs)[::-1]:
-        if isinstance(msg['content'], str):
-            last_message_content = msg['content']
+    model = get_param(rjson, "model", 'gpt-3.5-turbo')
+    temperature = get_param(rjson, "temperature", .8)
+    max_tokens = get_param(rjson, "max_tokens", 1100)
+    top_p = get_param(rjson, "top_p", 1.)
+    frequency_penalty = get_param(rjson, "frequency_penalty", .5)
+    presence_penalty = get_param(rjson, "presence_penalty", 0.0)
+
+    ai_text = ''
+    for resp in openai.ChatCompletion.create(
+            model=model,
+            messages=msgs,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            stream=True):
+        delta = resp["choices"][0]["delta"]
+        content = delta["content"] if "content" in delta else ""
+        ai_text += content
+        content = content.replace("\n", "[ENTRY]")
+        yield f'data: {content}\n\n'
+        conversation = conv_service.get_by_id(cid)
+        if conversation["stopGenerating"]:
             break
-
-    query = last_message_content
-    chosen_sections = []
-    my = search_docs(df_bills, query)
-    count = 0
-    sz = []
-    doc_link = ''
-    doc_name = '请参考以下链接：'
-    doc_links = []
-    for section_index in my:
-        if section_index == "KeyMessage":
-            for j in my[section_index]:
-                chosen_sections.append(SEPARATOR + j.replace("\n*", " "))
-        elif section_index == "Doc_Name":
-            for j in my[section_index]:
-                if j in sz:
-                    a = 1
-                else:
-                    sz.append(j)
-                    doc_links.append("<a href='{}'>{}</a>".format(my['Doc_Link'][count], j))
-                count = count + 1
-
-    doc_link = "  |  ".join(doc_links)
-
-    answer = answer_query_with_context(query, df, document_embeddings, chosen_sections, doc_name, doc_link)
-    ai_text = answer
-    func(ai_text)
-    content = ai_text.replace("\n", "[ENTRY]")
-    yield f'data: {content}\n\n'
     yield "data: [DONE]\n\n"
-
-    conversation = conv_service.get_by_id(cid)
-    if conversation["stopGenerating"]:
-        yield "data: [DONE]\n\n"
-        return
-
-# 构造文本搜索提示
-def answer_query_with_context(
-        query: str,
-        df: pd.DataFrame,
-        document_embeddings: Dict[Tuple[str, str], np.ndarray],
-        chosen_sections: List[str],
-        doc_name: str,
-        doc_link: str,
-        show_prompt: bool = False
-) -> str:
-    header = """Answer the question as truthfully as possible using the provided context, and if the answer is not
-    contained within the text below, say "I don't know."\n\nContext:\n """
-
-    # 将chosen_sections拆分成多个子列表
-    MAX_CONTEXT_LENGTH = 990
-    section_groups = []
-    current_group = []
-    current_length = 0
-    for section in chosen_sections:
-        section_length = len(section)
-        if current_length + section_length > MAX_CONTEXT_LENGTH:
-            section_groups.append(current_group)
-            current_group = []
-            current_length = 0
-        current_group.append(section)
-        current_length += section_length
-    if current_group:
-        section_groups.append(current_group)
-
-    # 对每个子列表进行文本拼接并进行OpenAI API调用
-    found_answer = False
-    answer = ""
-    for section_group in section_groups:
-        if found_answer:
-            break
-        prompt = header + "".join(section_group) + "\n\n Q: " + query + "\n A:"
-        response = openai.Completion.create(
-            prompt=prompt,
-            engine=TEXT,
-            **COMPLETIONS_API_PARAMS
-        )
-        answer = response["choices"][0]["text"].strip(" \n")
-        if answer != "I don't know.":
-            found_answer = True
-
-    # 拼接所有结果并返回
-    if not found_answer:
-        doc_link = ''
-        doc_name = ''
-        search_result = openai.Completion.create(
-            prompt=query,
-            engine=TEXT,
-            max_tokens=1000,
-            n=1,
-            stop=None,
-            temperature=0.5
-        )
-        answer = search_result.choices[0].text.strip()
-
-    return answer + doc_name + doc_link
+    func(ai_text)
 
 
-# 保存日志文件
 def init_logging():
     logfile_name = './chat.log'
     logging.basicConfig(level=logging.INFO, filename=logfile_name,
@@ -413,17 +218,13 @@ def init_logging():
                         datefmt="%F %T")
 
 
-# 初始化mongodb数据库
 def init_database():
     logging.info(sys.argv[1:])
     opts, args = getopt.getopt(sys.argv[1:], 'u:p:', ["host=", "port=", "databaseName="])
     set_database(opts)
 
 
-#运行程序
 if __name__ == '__main__':
     init_logging()
     init_database()
     app.run(host="0.0.0.0", port=8383, debug=False)
-
-
